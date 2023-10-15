ab0236a229f03a002765bedc5ba2f91da075bf65
bennetthamilton <bennetthamilton0@gmail.com>
2023-10-15 10:04:58 -0700

    variable names should be clear, removed redundant comment since now its clear what it does

--- Exercises/Exercise2/main.rb
+++ Exercises/Exercise2/main.rb
@@ -11,11 +11,11 @@
 # Mangle the name by reversing it or borgifying it
 def modified_name(choose)
 
-  # Split the name into first and last in an array
-  split_name = @name.split
+  first_last_name = @name.split
 
   # Reverse the array of name components so the array is [last, first]
   reversed_name_components = split_name.reverse
+
   # Rejoin the array of name components
   new_name1 = reversed_name_components.join(' ')
   # Store the mangled name
