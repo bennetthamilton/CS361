459d738fa7d72877b2b0fa13bf8a4e9b74f8062e
bennetthamilton <bennetthamilton0@gmail.com>
2023-10-21 12:17:06 -0700

    schedule needs quarter id to get the quarter, use that to get the max number of courses

--- Exercises/Exercise3/main.rb
+++ Exercises/Exercise3/main.rb
@@ -13,9 +13,7 @@ class Student
   end
 
   def add_to_schedule(course, schedule, quarter_id)
-    quarter = schedule.quarter[quarter_id]
-    course_list = quarter.course_list
-    max_courses = course_list.maximum_number_of_courses
+    max_courses = schedule[quarter_id].maximum_number_of_courses
 
     if max_courses < MAX_COURSES
         course_list.add(course)
