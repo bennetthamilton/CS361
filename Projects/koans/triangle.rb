51da8ee1766692ac9018e43e84960ab348e867f0
bennetthamilton <bennetthamilton0@gmail.com>
2023-10-25 10:24:53 -0700

    finished triangle project2

--- Projects/koans/triangle.rb
+++ Projects/koans/triangle.rb
@@ -14,6 +14,10 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  if a <= 0 || b <= 0 || c <= 0 || (a + b <= c) || (a + c <= b) || (b + c <= a)
+    raise TriangleError
+  end
+
   if a == b && b == c
     :equilateral
   elsif a == b || b == c || a == c
