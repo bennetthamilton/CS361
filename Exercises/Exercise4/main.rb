03c86483f91e4fca6da482c5f7742a8296428cdb
bennetthamilton <bennetthamilton0@gmail.com>
2023-10-29 09:59:33 -0700

    used clean code prrinciples of using exceptions to write better error handling for part 1

--- Exercises/Exercise4/main.rb
+++ Exercises/Exercise4/main.rb
@@ -5,33 +5,45 @@
 # Description: practice using error and boundary principles from Martin's Clean Code
 
 # Exercise 4 Part 1 (Exception Handling)
+# ref: https://stackify.com/rescue-exceptions-ruby/
 
 class MentalState
   def auditable?
     # true if the external service is online, otherwise false
   end
   def audit!
-    # Could fail if external service is offline
+    # raise an exception if the external service is offline
+    unless auditable?
+      raise ExternalServiceError, "External service is offline"
+    end
   end
   def do_work
     # Amazing stuff...
   end
 end
 
+# create exception class to be raised if external service is offline (inherits from StandardError)
+# ref: https://ruby-doc.org/core-2.5.1/StandardError.html
+# ref: https://thixalongmy.haugiang.gov.vn/media/1175/clean_code.pdf
+# Chapter 7 advices against "return code" to handle errors begin/rescue (aka try/except) instead
+class ExternalServiceError < StandardError; end
+
 def audit_sanity(bedtime_mental_state)
-  return 0 unless bedtime_mental_state.auditable?
-  if bedtime_mental_state.audit!.ok?
-    MorningMentalState.new(:ok)
-  else 
-    MorningMentalState.new(:not_ok)
+  begin
+    new_state = bedtime_mental_state.audit!
+
+    if new_state.ok?
+      MorningMentalState.new(:ok)
+    else
+      MorningMentalState.new(:not_ok)
+    end
+  rescue ExternalServiceError => e
+    puts "Error: #{e.message}"
   end
 end
 
-if audit_sanity(bedtime_mental_state) == 0
-  puts "error"
-else
-  new_state = audit_sanity(bedtime_mental_state)
-end
+bedtime_mental_state = MentalState.new          # create object
+new_state = audit_sanity(bedtime_mental_state)  # audit mental state
 
 
 
