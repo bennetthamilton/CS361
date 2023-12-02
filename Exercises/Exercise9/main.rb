df5b6e1daf15dad2101303cc714ac12c0b02608b
bennetthamilton <bennetthamilton0@gmail.com>
2023-12-02 11:25:40 -0800

    added initial interface with error checking

---
+++ Exercises/Exercise9/main.rb
@@ -0,0 +1,78 @@
+# Title: Exercise 9: Interfaces
+# Name: Bennett Hamilton
+# Username: bennetthamilton
+# Date: 12/2/23
+# Description: modify given code to practice principles of OOD and interfacing
+
+# common interface for bike classes
+# ref: https://www.geeksforgeeks.org/ruby-module/
+module Bike
+
+  def prepare
+    raise NotImplementedError, "#{self.class} must implement prepare method"
+  end
+
+end
+
+class Employee
+
+  # def prepare(bike)
+  #   if bike.class == BmxBike
+  #     bike.clean
+  #   elsif bike.class == RoadBike
+  #     bike.lubricate_gears
+  #   elsif bike.class == MountainBike
+  #     bike.adjust_suspension
+  #   elsif bike.class == Tricycle
+  #     bike.adjust_seat
+  #   else
+  #     puts 'Uhh, boss, I dunno how to do that.'
+  #   end
+  # end
+  def prepare(bike)
+    bike.prepare
+  rescue NotImplementedError => e
+    puts "#{e.message}"
+  end
+
+end
+
+class BmxBike
+
+  def clean
+    puts 'Cleaning...'
+  end
+
+end
+
+class RoadBike
+
+  def lubricate_gears
+    puts 'Lubricating gears...'
+  end
+
+end
+
+class MountainBike
+
+  def adjust_suspension
+    puts 'Adjusting suspension...'
+  end
+
+end
+
+class Tricycle
+
+  def adjust_seat
+    puts 'Adjusting seat...'
+  end
+
+end
+
+bikes = [BmxBike.new, RoadBike.new, MountainBike.new, Tricycle.new]
+
+employee = Employee.new
+
+bikes.each do |bike|
+  employee.prepare(bike)
+end
